a377ccb5d64f87bfe6dd7a3661b490b6cf71d98c
shubham singh <singhshubham68738@gmail.com>
2025-11-16 10:50:24 +0530

    working

--- Array/Apna_College_Aiigment/Question.java
+++ Array/Apna_College_Aiigment/Question.java
@@ -62,6 +62,13 @@ public class Question {
         return max;
     }
 
+    // Given an integer array nums, return all the triplets [nums[i], nums[j],
+    // nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j]
+    // +nums[k]== 0.
+    // Notice that the solution set must not contain duplicate triplets.
+    // Example 1:
+    // Input: nums = [-1, 0, 1, 2, -1, -4]
+    // Output: [ [-1, -1, 2] , [-1, 0, 1] ]
     public List<ArrayList<Integer>> triplets(int[] array) {
         HashSet<ArrayList<Integer>> result = new HashSet<>();
         for (int i = 0; i < array.length; i++) {
